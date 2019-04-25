9e147cb646056dc20541bd340825f3fe4c421f82
Roberto Stagi <r.stagi96@gmail.com>
2019-04-25 19:52:45 +0200

    correzione

--- include/PolitoceanExceptions.hpp
+++ include/PolitoceanExceptions.hpp
@@ -12,12 +12,6 @@ public:
 
     exception(const char* what_arg) : std::exception(), m(what_arg) {}
 
-protected:
-    static std::string name(const std::string& ename, int id_)
-    {
-        return "[Politocean.exception." + ename + "." + std::to_string(id_) + "] ";
-    }
-
 private:
     /// an exception object as storage for error messages
     std::runtime_error m;
@@ -28,12 +22,6 @@ private:
 class mqttException : public exception {
 public:
     mqttException(const char* what_arg) : exception(what_arg) {}
-
-protected:
-    static std::string name(const std::string& ename, int id_)
-    {
-        return "[Politocean.mqttException." + ename + "." + std::to_string(id_) + "] ";
-    }
 };
 
 
