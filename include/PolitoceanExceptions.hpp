3a6e22e20a356e06282304ad3e086beff2c295be
Roberto Stagi <r.stagi96@gmail.com>
2019-04-25 18:52:26 +0200

    correzioni

--- include/PolitoceanExceptions.hpp
+++ include/PolitoceanExceptions.hpp
@@ -1,7 +1,7 @@
 
 namespace Politocean {
 
-class exception : std::exception {
+class exception : public std::exception {
 public:
     /// returns the explanatory string
     const char* what() const noexcept override
@@ -23,20 +23,15 @@ private:
 };
 
 
-class mqttException : exception {
+class mqttException : public exception {
 public:
-    mqttException(const char* what_arg) : exception(what_arg), m(what_arg) {}
+    mqttException(const char* what_arg) : exception(what_arg) {}
 
 protected:
     static std::string name(const std::string& ename, int id_)
     {
         return "[Politocean.mqttException." + ename + "." + std::to_string(id_) + "] ";
     }
-
-
-private:
-    /// an exception object as storage for error messages
-    std::runtime_error m;
 };
 
 }
