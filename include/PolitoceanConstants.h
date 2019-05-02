ce8eba66b17bd2e4f02dfddfdb3238aa52bfe349
Angelo Pettinelli <angelo.pettinelli@studenti.polito.it>
2019-05-03 00:24:11 +0200

    Added commands buttons and axes mapping
    Added some style

--- include/PolitoceanConstants.h
+++ include/PolitoceanConstants.h
@@ -3,64 +3,89 @@
 
 #include <string>
 
-#define N_BUTTONS 8
+namespace Politocean
+{
+    using namespace std;
 
-namespace Politocean {
-using namespace std;
-    namespace Constants {
-        
-        namespace Topics {
-
+    namespace Constants
+    {
+        namespace Topics
+        {
             const string ERRORS { "errors/" };
-            const string INFO { "info/" };
+            const string INFO   { "info/" };
 
-            const string HMI { "hmi/" };
-            const string MICRO_ROV { "microRov/" };
+            const string MICRO_ROV  { "microRov/" };
+            const string MICRO_ROV_CAMERA { MICRO_ROV + string("camera") };
 
-            const string JOYSTICK { HMI + string("joystick/") };
+            const string HMI                { "hmi/" };
+            const string JOYSTICK           { HMI + string("joystick/") };
+            const string JOYSTICK_AXIS      { JOYSTICK + string("axis/") };
+            const string JOYSTICK_BUTTONS   { JOYSTICK + string("buttons/") };
+
             const string VISION { HMI + string("vision/") };
-
-            const string JOYSTICK_AXIS { JOYSTICK + string("axis/") };
-            const string JOYSTICK_BUTTONS { JOYSTICK + string("buttons/") };
 
             const string AUTODRIVE { VISION + string("autodrive") };
 
-            const string MICRO_ROV_CAMERA { MICRO_ROV + string("camera") };
         }
 
-
-
-        namespace Logger {
-
+        namespace Logger
+        {
             const string LOGS_PATH { "logs/" };
 
-            namespace Levels {
-                const string ERROR { "error" };
-                const string INFO { "info" };
-                const string DEBUG { "debug" };
+            namespace Levels
+            {
+                const string ERROR  { "error" };
+                const string INFO   { "info" };
+                const string DEBUG  { "debug" };
             }
         }
         
-        namespace MicroRov {
-            const string HOST_ADDRESS { "127.0.0.1" }; //TODO da togliere
-            const string PUBLISHERID { "MicroRov_Publisher" }; //TODO da cambiare
-            const string IP_ADDRESS { "10.0.0.3" };
+        namespace MicroRov
+        {
+            const string HOST_ADDRESS   { "127.0.0.1" }; //TODO da togliere
+            const string PUBLISHERID    { "MicroRov_Publisher" }; //TODO da cambiare
+            const string IP_ADDRESS     { "10.0.0.3" };
         }
 
-        namespace Hmi {
-            const string CLIENT_ID { "hmi_core" };
-            const string AUTODRIVE_ID { "autodrive" };
-            const string UI_ID { "ui" };
+        namespace Hmi
+        {
+            const string CLIENT_ID      { "hmi_core" };
+            const string AUTODRIVE_ID   { "autodrive" };
+            const string UI_ID          { "ui" };
 
-            const string IP_ADDRESS { "10.0.0.1" };
-            const string CAMERA_IP_ADDRESS { "10.0.0.4" };
+            const string IP_ADDRESS         { "10.0.0.1" };
+            const string CAMERA_IP_ADDRESS  { "10.0.0.4" };
         }
 
         namespace Rov {
-            const string TOPIC_PREFIX { "rov/" };
-            const string CLIENT_ID { "rov_core" };
+            const string TOPIC_PREFIX   { "rov/" };
+            const string CLIENT_ID      { "rov_core" };
 
             const string IP_ADDRESS { "10.0.0.2" };
+        }
+
+        namespace Commands
+        {
+            namespace Buttons
+            {
+                static const int MOTORS_ON   = 1;
+                static const int MOTORS_OFF  = 2;
+                static const int VDOWN       = 5;
+                static const int WRIST       = 7;
+                static const int RESET       = 9;
+                static const int VUP         = 14;
+                static const int MEDIUM_FAST = 24;
+                static const int SLOW        = 25;
+            }
+            
+            namespace Axes
+            {
+                static const int X          = 0;
+                static const int Y          = 1;
+                static const int SHOULDER   = 2;
+                static const int WRIST      = 4;
+                static const int RZ         = 5;
+            }
         }
 
     }
