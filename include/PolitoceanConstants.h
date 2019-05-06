7176af3a436a937723d2b4366e18328d2a43b1ae
Angelo Pettinelli <angelo.pettinelli@studenti.polito.it>
2019-05-06 14:55:20 +0200

    Added Pinout

--- include/PolitoceanConstants.h
+++ include/PolitoceanConstants.h
@@ -11,7 +11,13 @@ namespace Politocean
 
     namespace Constants
     {
-        const string CLIENT_ID_REGEX { "[\\w_]+" };
+        const string CLIENT_ID_REGEX = "[\\w_]+";
+
+        namespace Pinout
+        {
+            static const int RESET  = 7;
+            static const int MOTORS = 12;
+        }
 
         namespace Topics
         {
