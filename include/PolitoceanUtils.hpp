3a41c3e3d8addff6cd7e9c17551038bf7f0aa15c
sudo-rf <davidecota1@gmail.com>
2019-05-17 18:13:04 +0200

    fix include

--- include/PolitoceanUtils.hpp
+++ include/PolitoceanUtils.hpp
@@ -1,5 +1,6 @@
 #include <climits>
 #include <nlohmann/json.hpp>
+#include "../libs/Publisher/Publisher.h"
 
 #define ABS(N) ((N<0)?(-N):(N))
 
@@ -9,7 +10,7 @@ static long map(long x, long in_min, long in_max, long out_min = 0, long out_max
     return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
-static void publish(Publisher&git publisher, const std::string& topic, const std::string& status)
+static void publish(Publisher& publisher, const std::string& topic, const std::string& status)
 {
     nlohmann::json json;
 
