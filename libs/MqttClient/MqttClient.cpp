e7ebfb3729a275918c777e11659115863feaea2a
Roberto Stagi <r.stagi96@gmail.com>
2019-05-26 12:40:47 +0200

    added so libraries

--- libs/MqttClient/MqttClient.cpp
+++ libs/MqttClient/MqttClient.cpp
@@ -1,8 +1,10 @@
 #include "MqttClient.h"
 #include <logger.h>
+#include <sstream>
 
 using namespace Politocean;
 using namespace std;
+
 
 MqttClient::MqttClient(const std::string& clientID, const std::string& address, const int& port)
     :  mosqpp::mosquittopp(clientID.c_str()), clientID_(clientID), address_(address)
@@ -10,6 +12,7 @@ MqttClient::MqttClient(const std::string& clientID, const std::string& address, 
     port_ = port;
     mosqpp::lib_init();
 }
+
 
 MqttClient::~MqttClient()
 {
@@ -29,23 +32,18 @@ void MqttClient::connect()
     loop_start();
 }
 
-/**
- * Wait until client is connected
- */
+
 void MqttClient::wait()
 {
 	while(is_connected());
 }
 
-/**
- * Subscribe to the given topic with the given callback.
- * Remove traling '/'.
- */
+
 void MqttClient::subscribeTo(const std::string& topic, callback_t pf)
 {
 	string topicf = topic;
 	if(topicf[topicf.size()-1]=='/')
-		topicf = topicf.substr(0, topicf.find_last_not_of('/')+1)+"/"; //trim trailing '/' if they exist
+		topicf = topicf.substr(0, topicf.find_last_not_of(" /")+1)+"/"; //trim trailing '/' if they exist
 
 	topic_to_callback.insert(std::pair<std::string, callback_t>(topicf, pf));
 
@@ -54,6 +52,7 @@ void MqttClient::subscribeTo(const std::string& topic, callback_t pf)
 	logger::log(logger::DEBUG, string("Subscribed ")+clientID_+string(" to topic ")+topic);
 }
 
+
 void MqttClient::subscribeTo(const std::string& topic, void (*pf)(const std::string& payload))
 {
 	callback_t wrapper_function = [pf](const std::string& payload, const std::string& topic) { (*pf)(payload); };
@@ -61,14 +60,13 @@ void MqttClient::subscribeTo(const std::string& topic, void (*pf)(const std::str
 	subscribeTo(topic, wrapper_function);
 }
 
+
 void MqttClient::publish(const string& topic, const string& message)
 {
     mosqpp::mosquittopp::publish(NULL, topic.c_str(), message.length(), message.c_str(), this->qos, false);
 }
 
-/**
- * Unsubscribe from the given topic.
- */
+
 void MqttClient::unsubscribeFrom(const std::string& topic)
 {
 	topic_to_callback.erase(topic);
@@ -78,15 +76,15 @@ void MqttClient::unsubscribeFrom(const std::string& topic)
 	logger::log(logger::DEBUG, string("Unsubscribed ")+clientID_+string(" from topic ")+topic);
 }
 
+
 void MqttClient::unsubscribeFrom(const std::vector<std::string>& topics)
 {
 	for (auto &topic : topics)
 		unsubscribeFrom(topic);
 }
 
-/**
- * get the topics which it's subscribed to
- */
+
+
 std::vector<string> MqttClient::getSubscribedTopics()
 {
 	std::vector<std::string> topics;
@@ -96,15 +94,15 @@ std::vector<string> MqttClient::getSubscribedTopics()
 	return topics;
 }
 
-/**
- * get client id
- */
-std::string MqttClient::getClientId(){
+
+std::string MqttClient::getClientId()
+{
     return clientID_;
 }
 
 
-void MqttClient::on_message(const struct mosquitto_message *msg) {
+void MqttClient::on_message(const struct mosquitto_message *msg)
+{
     if(topic_to_callback.empty())
 		return;
 	
@@ -141,32 +139,47 @@ void MqttClient::on_message(const struct mosquitto_message *msg) {
 	callback((char*)msg->payload, msg->topic);
 }
 
+
 void MqttClient::disconnect()
 {
     mosquittopp::disconnect();
-}
-
-void MqttClient::on_subscribe(int, int, const int *) {
-    cout << TAG <<"Subscription succeeded." << endl;
-}
-
-void MqttClient::on_disconnect(int rc)
-{
-    cout << TAG << "disconnection(" << rc << ")" << endl;
 	connected = false;
 }
 
+
+void MqttClient::on_subscribe(int, int, const int *)
+{
+	stringstream ss;
+    ss << TAG << "Subscription succeeded.";
+	logger::log(logger::DEBUG, ss.str());
+}
+
+
+void MqttClient::on_disconnect(int rc)
+{
+	stringstream ss;
+    ss << TAG << "disconnection (" << rc << "). Reconnecting...";
+	logger::log(logger::ERROR, ss.str());
+}
+
+
 void MqttClient::on_connect(int rc)
 {
+	stringstream ss;
     if ( rc == 0 ) {
-        cout << TAG << "connected with server" << endl;
+        ss << TAG << "connected with server";
+		logger::log(logger::DEBUG, ss.str());
 		connected = true;
     } else {
-        cout << TAG << "impossible to connect with server(" << rc << ")" << endl;
+		ss << TAG << "impossible to connect with server(" << rc << ")";
+		logger::log(logger::ERROR, ss.str());
     }
 }
 
+
 void MqttClient::on_publish(int mid)
 {
-    cout << TAG << "Message (" << mid << ") succeed to be published " << endl;
+	stringstream ss;
+	ss << TAG << "Message (" << mid << ") succeed to be published ";
+	logger::log(logger::DEBUG, ss.str());
 }
