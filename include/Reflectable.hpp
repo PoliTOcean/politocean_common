16f996cb0fbfffeae12f3f163090eafce9beb8f7
pettinz <angelo.pettinelli@icloud.com>
2019-06-07 22:53:08 +0200

    Switched to MQTT namespace and changed exception class

--- include/Reflectable.hpp
+++ include/Reflectable.hpp
@@ -1,41 +1,42 @@
 #ifndef REFLECTABLE_H
 #define REFLECTABLE_H
 
-#include <string>
 #include <iostream>
+#include <string>
+#include <exception>
 
-namespace Reflectable {
-
-class NotImplementedException : public std::exception {
-    /// an exception object as storage for error messages
-    std::runtime_error m;
-
-public:
-    /// returns the explanatory string
-    const char* what() const noexcept override
+namespace MQTT
+{
+    class NotImplementedException : public std::exception
     {
-        return m.what();
-    }
+        std::string msg_;
 
-    NotImplementedException(const std::string& msg) : NotImplementedException(msg.c_str()) {}
-    NotImplementedException(const char *msg) : std::exception(), m(msg) {}
-};
+    public:
+        NotImplementedException(const std::string& msg) : msg_(msg) {}
 
-class Reflectable {
-public:
-    template <typename T, typename std::enable_if<std::is_base_of<Reflectable, T>::value>::type* = nullptr>
-    static T parse(std::string stringified) {
-        throw NotImplementedException("Method PARSE has not been implemented.");
-        T obj;
-        return obj;
-    }
-
-    virtual std::string stringify() {
-        throw NotImplementedException("Method STRINGIFY has not been implemented.");
-        return "";
-    }
-};
-
+        virtual char const* what() const throw()
+        {
+            return msg_.c_str();
+        }
+    };
+    
+    class Reflectable
+    {
+    public:
+        template <typename T, typename std::enable_if<std::is_base_of<Reflectable, T>::value>::type* = nullptr>
+        static T parse(std::string stringified)
+        {
+            throw NotImplementedException("Method PARSE has not been implemented.");
+            T obj;
+            return obj;
+        }
+        
+        virtual std::string stringify()
+        {
+            throw NotImplementedException("Method STRINGIFY has not been implemented.");
+            return "";
+        }
+    };
 }
 
 #endif
