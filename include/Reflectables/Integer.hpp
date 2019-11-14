255cb137cb2511da737186d9678bb584ad11d102
pettinz <angelo.pettinelli@icloud.com>
2019-11-14 21:45:15 +0100

    Formatted

--- include/Reflectables/Integer.hpp
+++ include/Reflectables/Integer.hpp
@@ -6,8 +6,10 @@
 
 #include "../Reflectable.hpp"
 
-namespace Politocean {
-namespace Types {
+namespace Politocean
+{
+namespace Types
+{
 
 using namespace Reflectable;
 
@@ -18,22 +20,22 @@ class Integer : public IReflectable
 public:
     Integer(int value) : value_(value) {}
 
-    static Integer parse(const std::string& stringified) override
+    static Integer parse(const std::string &stringified)
     {
         int value;
         try
         {
             value = std::stoi(stringified);
         }
-        catch (const std::exception& e)
+        catch (const std::exception &e)
         {
-            throw ReflectableParsingException(std::string("An error occurred parsing Integer value: ")+e.what());
+            throw ReflectableParsingException(std::string("An error occurred parsing Integer value: ") + e.what());
         }
         catch (...)
         {
             throw ReflectableParsingException("An error occurred parsing Integer value.");
         }
-        
+
         return Integer(value);
     }
 
@@ -47,23 +49,24 @@ public:
         return std::to_string(value_);
     }
 
-    int getInt() {
+    int getInt()
+    {
         return value_;
     }
 
-    void operator= (const int& i)
+    void operator=(const int &i)
     {
         value_ = i;
     }
-    
-    friend std::ostream& operator<< (std::ostream& output, const Integer& I)
+
+    friend std::ostream &operator<<(std::ostream &output, const Integer &I)
     {
         output << I.value_;
-        return output;            
+        return output;
     }
 
-    Integer& operator++ () { ++value_; }
-    Integer operator++ (int)
+    Integer &operator++() { ++value_; }
+    Integer operator++(int)
     {
         Integer I(value_);
         ++value_;
@@ -71,28 +74,28 @@ public:
         return I;
     }
 
-    Integer& operator-- () { --value_; }
-    Integer operator-- (int)
-    { 
+    Integer &operator--() { --value_; }
+    Integer operator--(int)
+    {
         Integer I(value_);
         --value_;
-        
+
         return I;
     }
 
-    Integer& operator+= (const Integer& rhs) { value_ += rhs.value_; }
-    Integer& operator-= (const Integer& rhs) { value_ -= rhs.value_; }
-    
-    inline friend bool operator< (const Integer& lhs, const Integer& rhs) { return (lhs.value_ < rhs.value_); }
-    inline friend bool operator> (const Integer& lhs, const Integer& rhs) { return rhs < lhs; }
-    inline friend bool operator<= (const Integer& lhs, const Integer& rhs) { return !(lhs > rhs); }
-    inline friend bool operator>= (const Integer& lhs, const Integer& rhs) { return !(lhs < rhs); }
+    Integer &operator+=(const Integer &rhs) { value_ += rhs.value_; }
+    Integer &operator-=(const Integer &rhs) { value_ -= rhs.value_; }
 
-    inline friend bool operator== (const Integer& lhs, const Integer& rhs){ return (lhs.value_ == rhs.value_); }
-    inline friend bool operator!= (const Integer& lhs, const Integer& rhs){ return !(lhs == rhs); }
+    inline friend bool operator<(const Integer &lhs, const Integer &rhs) { return (lhs.value_ < rhs.value_); }
+    inline friend bool operator>(const Integer &lhs, const Integer &rhs) { return rhs < lhs; }
+    inline friend bool operator<=(const Integer &lhs, const Integer &rhs) { return !(lhs > rhs); }
+    inline friend bool operator>=(const Integer &lhs, const Integer &rhs) { return !(lhs < rhs); }
+
+    inline friend bool operator==(const Integer &lhs, const Integer &rhs) { return (lhs.value_ == rhs.value_); }
+    inline friend bool operator!=(const Integer &lhs, const Integer &rhs) { return !(lhs == rhs); }
 };
 
-}
-}
+} // namespace Types
+} // namespace Politocean
 
 #endif // REFLECTABLE_INTEGER_H
